b9a040a3de342d8f9fca6d42a27a430e22b78e0d
Michealtimi <michealagunbiade1@gmail.com>
2024-02-20 03:02:54 -1100

    first push

---
+++ app/collection/hero.tsx
@@ -0,0 +1,15 @@
+import { Clock, Mail, Search } from 'lucide-react'
+import Link from 'next/link';
+import React from 'react';
+import Header from '@/app/hero-section/header';
+
+
+export default function Hero() {
+  return (
+    <>
+    <div className='flex background-image: //////////'>
+
+    </div>
+    </>
+  )
+}
